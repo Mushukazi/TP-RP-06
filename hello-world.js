4fbd91d0a57dd972f3baa135b519fbf89c426675
Mushukazi <musharafkazi65@gmail.com>
2024-04-25 19:36:55 +0530

    5 times hello world print

---
+++ hello-world.js
@@ -0,0 +1,15 @@
+console.log("hello world")
+
+var a = "Hello World";
+
+for (let b=0; b<5; b++){
+    console.log(b,a)
+}
+
+// or
+
+console.log(20+15);
+console.log(20+30);
+console.log(20+12);
+console.log(20+25);
+console.log(20+10);
